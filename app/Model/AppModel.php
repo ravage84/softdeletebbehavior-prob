6ea9b3f65d89f2a7e665931603ad7094d74bee24
Marc Würth <ravage@bluewin.ch>
2015-06-05 00:08:23 +0200

    Add missing methods to AppModel.php, which don't matter for the problem at hand

--- app/Model/AppModel.php
+++ app/Model/AppModel.php
@@ -36,4 +36,20 @@ class AppModel extends Model {
 		'Containable'
 	);
 
+	public function exists($id = null) {
+		if ($this->Behaviors->attached('SoftDelete')) {
+			return $this->existsAndNotDeleted($id);
+		} else {
+			return parent::exists($id);
+		}
+	}
+
+	public function delete($id = null, $cascade = true) {
+		$result = parent::delete($id, $cascade);
+		if ($result === false && $this->Behaviors->enabled('SoftDelete')) {
+			return (bool)$this->field('deleted', array('deleted' => 1));
+		}
+		return $result;
+	}
+
 }
